4243d4c99aa4ea462c55fba04f0211da8e82f81b
Abu Bakkar <59757268+AbuBakkar-l@users.noreply.github.com>
2025-01-26 21:17:34 +0500

    login

--- js/script.js
+++ js/script.js
@@ -7,5 +7,16 @@ $(document).ready(function () {
   });
   $('#filter_button').click(function(){
     $('.filter_menu').toggle();
-  })
+  });
+  $("#popup-trigger").click(function () {
+    $("#popup").show(); // Show the popup
 });
+
+// Bind the close button click event outside of the trigger
+$(".close").click(function () {
+  console.log("Close button clicked");
+    $("#popup").hide(); // Hide the popup
+});
+  
+});
+

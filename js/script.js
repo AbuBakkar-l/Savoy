59413a9d198aa01b4faef838a7f9d98e88f9b911
Abu Bakkar <59757268+AbuBakkar-l@users.noreply.github.com>
2025-01-27 19:38:53 +0500

    cartProduct Detail

--- js/script.js
+++ js/script.js
@@ -15,21 +15,12 @@ $(".close").click(function () {
     $("#popup").hide(); 
 });
   // cart 
-  let isVisible = false;
   $("#trigger-cart").click(function () {
-      const content = $(".cart-content");
-      if (!isVisible) {
-        content.css("right", "0px" ); 
-          isVisible = true;
-      } else {
-        content.css("right", "-300px" ); 
-          isVisible = false;
-      }
-  });
-  $("#cart-close").click(function () {
+    const content = $(".cart-content");
+    content.show('fast');
+});
+  $(".cart-close").click(function () {
     $('.cart-content').hide('fast');
-    // const content = $(".cart-content");
-    //   content.css("right", "-300px" ); 
     }
   );
 });
